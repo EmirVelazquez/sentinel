000298d057d98d172d5d665837ccbe579fa9e627
Justin Cheng <ijustin17@gmail.com>
2020-02-21 14:19:41 -0600

    added final styling to the front end UI for the Login and Sign Up scenes. Added functionality to let users see passwords and state stores all the data that the user inputs correctly

--- App.js
+++ App.js
@@ -6,12 +6,3 @@ import { render } from "react-dom";
 export default function App() {
   return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,14 +1,140 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import Styles from "../css/styles";
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  KeyboardAvoidingView
+} from "react-native";
 
-const SignUp = () => {
-  return (
-    <View style={Styles.container}>
-      <Text style={Styles.header}>Sign Up</Text>
-    </View>
-  );
-};
-const styles = StyleSheet.create({});
+import Styles from "../css/styles";
+import Separator from "./Separator";
+import { ScrollView } from "react-native-gesture-handler";
+
+class SignUp extends Component {
+  // Retrieving input data
+  state = {
+    signUpEmail: "",
+    signUpPassword: "",
+    signUpConfirm: "",
+    hidePassword: true
+  };
+
+  //Individual onChange handlers for each part of state
+  //=========================================================
+  handleSignupEmailChange = event => {
+    console.log(event);
+    this.setState({
+      signUpEmail: event
+    });
+  };
+  handleSignUpPasswordChange = event => {
+    console.log(event);
+    this.setState({
+      signUpPassword: event
+    });
+  };
+  handleSignupConfirmChange = event => {
+    console.log(event);
+    this.setState({
+      signUpConfirm: event
+    });
+  };
+  //=========================================================
+
+  // State can be passed to the backend for auth -Justin
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({
+      signUpEmail: this.state.signUpEmail,
+      signUpPassword: this.state.signUpPassword,
+      signUpConfirm: this.state.signUpConfirm
+    });
+  };
+
+  managePasswordVisability = () => {
+    this.setState({ hidePassword: !this.state.hidePassword });
+  };
+
+  render() {
+    return (
+      <View style={Styles.container}>
+        <Text style={Styles.header}>Sign Up</Text>
+        <View style={Styles.innerContainer}>
+          <TextInput
+            name="signUpEmail"
+            placeholder="Email"
+            style={{
+              height: 40,
+              backgroundColor: "black",
+              borderColor: "gray",
+              borderTopWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderWidth: 2,
+              color: "white"
+            }}
+            onChangeText={this.handleSignupEmailChange}
+            signUpEmail={this.state.signUpEmail}
+          />
+
+          <Separator />
+
+          <TextInput
+            name="SignUpPassword"
+            placeholder="Password"
+            style={{
+              height: 40,
+              backgroundColor: "black",
+              borderColor: "gray",
+              borderTopWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderWidth: 2,
+              color: "white"
+            }}
+            onChangeText={this.handleSignUpPasswordChange}
+            signUpPassword={this.state.signUpPassword}
+            secureTextEntry={this.state.hidePassword}
+          />
+
+          <Separator />
+
+          <TextInput
+            name="signUpConfirm"
+            placeholder="Confirm Password"
+            style={{
+              height: 40,
+              backgroundColor: "black",
+              borderColor: "gray",
+              borderTopWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderWidth: 2,
+              color: "white"
+            }}
+            onChangeText={this.handleSignupConfirmChange}
+            signUpConfirm={this.state.signUpConfirm}
+            secureTextEntry={this.state.hidePassword}
+          />
+          <Separator />
+          <Separator />
+
+          <Button
+            title="Show Password"
+            color="black"
+            onPress={this.managePasswordVisability}
+          />
+        </View>
+
+        <Separator />
+
+        <Button title="Submit" onPress={this.handleFormSubmit} />
+      </View>
+    );
+  }
+}
 
 export default SignUp;
