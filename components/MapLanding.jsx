4c120a510ee3e725a9cc32ce5f5583d6d24f090c
Justin Cheng <ijustin17@gmail.com>
2020-02-27 20:44:25 -0600

    home pasge style fix

--- components/MapLanding.jsx
+++ components/MapLanding.jsx
@@ -4,19 +4,19 @@ import { Text, View, Slider, Button, KeyboardAvoidingView } from "react-native";
 import Styles from "../css/styles";
 import { ScrollView } from "react-native-gesture-handler";
 import * as Location from "expo-location";
-import * as Permissions from 'expo-permissions';
+import * as Permissions from "expo-permissions";
 
 class MapLanding extends Component {
-
   state = {
     location: {},
     region: {}
-  }
+  };
 
   componentDidMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
+    if (Platform.OS === "android" && !Constants.isDevice) {
       this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+        errorMessage:
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
       });
     } else {
       this._getLocationAsync();
@@ -25,20 +25,25 @@ class MapLanding extends Component {
 
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
+    if (status !== "granted") {
       this.setState({
-        errorMessage: 'Permission to access location was denied',
+        errorMessage: "Permission to access location was denied"
       });
     }
 
-    let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true
+    });
     this.setState({ location });
-    this.setState({ region: { latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude } })
-    console.log(this.state.region)
-    console.log(this.state.location)
-
+    this.setState({
+      region: {
+        latitude: this.state.location.coords.latitude,
+        longitude: this.state.location.coords.longitude
+      }
+    });
+    console.log(this.state.region);
+    console.log(this.state.location);
   };
-
 
   Emergency = event => {
     if (event == 0) {
@@ -53,19 +58,12 @@ class MapLanding extends Component {
       <KeyboardAvoidingView behavior="position">
         <ScrollView>
           <View style={Styles.mapContainer}>
-<<<<<<< HEAD
-            <MapView style={Styles.mapStyle}
-              provider={PROVIDER_GOOGLE}>
-              <Button style={Styles.Nav} title="Nav"></Button>
-=======
             <MapView style={Styles.mapStyle}>
               {/* <Button style={Styles.Nav} title="Nav"></Button> */}
->>>>>>> workBuild
             </MapView>
             <View style={Styles.textContainer}>
               <Text style={Styles.mapUI}>Your Family:</Text>
-              <View style={Styles.family}>
-              </View>
+              <View style={Styles.family}></View>
               <Slider
                 style={Styles.switch}
                 step={1}
