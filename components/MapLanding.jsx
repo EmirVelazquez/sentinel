bbfa37598e84184b72dca482f6c269900c2647b7
colemiller21 <cole.t.miller@gmail.com>
2020-02-27 18:56:47 -0600

    added location in maplanding

--- components/MapLanding.jsx
+++ components/MapLanding.jsx
@@ -3,8 +3,30 @@ import MapView from "react-native-maps";
 import { Text, View, Slider } from "react-native";
 import Styles from "../css/styles";
 import { ScrollView } from "react-native-gesture-handler";
+import { Location } from "expo-location";
+import { Permissions } from "expo-permissions";
 
 class MapLanding extends Component {
+
+  state = {
+    location: {}
+  }
+
+  getLocation = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (status !== "granted") {
+      console.log("not granted")
+    }
+
+    const location = await Location.getCurrentPositionAsync();
+
+    console.log(this.state.location)
+    this.setState({
+      location: this.state.location
+    })
+  }
+
   Emergency = event => {
     if (event == 0) {
       console.log("Changed to: " + event + " No Emergency");
@@ -12,6 +34,7 @@ class MapLanding extends Component {
       console.log("Changed to: " + event + " Emergency Logged");
     }
   };
+
   render() {
     return (
       <View style={Styles.mapContainer}>
