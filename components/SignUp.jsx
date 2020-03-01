db35ea7e0d130e078d7ba62b4b1eb64061a94140
MR-Stan <michaelrstanley87@gmail.com>
2020-03-01 00:18:39 -0600

    working on automatic login after signup

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -14,8 +14,18 @@ class SignUp extends Component {
     signUpLName: "",
     signUpEmail: "",
     signUpPassword: "",
-    hidePassword: true
+    hidePassword: true,
   };
+
+  storeToken = async (token) => {
+    try {
+      await AsyncStorage.setItem('jwt', token);
+      // Render map
+      Actions.MapLanding();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   signUp() {
     axios.post('https://sentinel-api.herokuapp.com/api/user',
@@ -26,12 +36,34 @@ class SignUp extends Component {
         pass: this.state.signUpPassword
       })
       .then(response => {
-        console.log(response);
-        // call login here to get token
-    })
+        if (response.status === 200) {
+          // need to create jwt and render map
+          // const { email, pass } = this.state;
+          // axios.post('https://sentinel-api.herokuapp.com/login/submit',
+          //   {
+          //     email,
+          //     pass
+          //   })
+          //   .then(res => {
+          //     console.log(res)
+          //     const token = res.data.token;
+          //     if (token) {
+          //       this.storeToken(token);
+          //     }
+          //     else {
+          //       console.log(token);
+          //       Actions.Home();
+          //     }
+          //   });
+        }
+        else {
+          // There was an error at sign up
+          console.log('Failed: ' + response.status);
+        }
+      })
       .catch(err => {
         console.log(err);
-    })
+      })
   }
 
   goToInformation = () => {
@@ -77,10 +109,6 @@ class SignUp extends Component {
       signUpPassword: this.state.signUpPassword
     })
     this.signUp()
-
-
-    // User info gets sent to database and is verified, then we send them to the maplanding page
-    Actions.MapLanding();
   };
 
   managePasswordVisability = () => {
